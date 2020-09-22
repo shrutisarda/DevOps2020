10745171dbbac233232bad5dc4d94db4d416e83b
shrutisardamind <shrutisardamind@shrutisardamind.internal.cloudapp.net>
2020-09-22 12:38:10 +0000

    Initial commit of Project

---
+++ AchiStar/src/com/simplilearn/devops/Calculate.java
@@ -0,0 +1,8 @@
+package com.simplilearn.devops;
+
+public class Calculate {
+public int sum(int x,int y)
+{
+	return x+y;
+}
+}
---
+++ AchiStar/src/com/simplilearn/devops/Sample.java
@@ -0,0 +1,11 @@
+package com.simplilearn.devops;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		Calculate cal= new Calculate();
+		cal.sum(4, 6);
+		System.out.println("Hello World!!!");
+	}
+
+}
